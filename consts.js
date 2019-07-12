d30a5b27238f9fe655b54df6cf335139b64c25c0
Nir Geller <nir@nir.laptop>
2019-07-12 15:56:38 +0300

    Added font suffixes to allowed suffixes

--- consts.js
+++ consts.js
@@ -1,5 +1,5 @@
 const ALLOWED_IMAGE_SUFFIXES = [".png", ".svg", ".jpg"];
-const ALLOWED_SUFFIXES = [".js", ".css"];
+const ALLOWED_SUFFIXES = [".js", ".css", "woff2", "woff", "ttf"];
 const BLACK_LIST = ["https://github"];
 const MAX_DEPTH = 50;
 const INCLUDE_IMAGES = false;
